38ef7807132a7e69ccf487d3fde759f77d002df6
Aigerim Beishenbekova <aykobb@gmail.com>
2018-04-02 21:36:14 +0600

    splash screen added

--- app/src/main/java/com/iko/android/weather/ui/splash/SplashActivity.java
+++ app/src/main/java/com/iko/android/weather/ui/splash/SplashActivity.java
@@ -1,7 +1,14 @@
 package com.iko.android.weather.ui.splash;
 
+import android.graphics.drawable.Animatable2;
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.graphics.drawable.Animatable2Compat;
+import android.support.graphics.drawable.AnimatedVectorDrawableCompat;
+import android.util.Log;
 
 import com.iko.android.weather.BR;
 import com.iko.android.weather.R;
@@ -18,11 +25,42 @@ public class SplashActivity extends BaseActivity<ActivitySplashBinding, SplashVi
 
     @Inject
     SplashViewModel viewModel;
+    private ActivitySplashBinding dataBinding;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         viewModel.setNavigator(this);
+        dataBinding = getDataBinding();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        animatedDrawable();
+    }
+
+    private void animatedDrawable(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            AnimatedVectorDrawable drawable = (AnimatedVectorDrawable)dataBinding.ivSun.getDrawable();
+            drawable.registerAnimationCallback(new Animatable2.AnimationCallback() {
+                @Override
+                public void onAnimationEnd(Drawable drawable) {
+                    openMainActivity();
+                }
+            });
+            drawable.start();
+        }else {
+            AnimatedVectorDrawableCompat drawable = (AnimatedVectorDrawableCompat) dataBinding.ivSun.getDrawable();
+            drawable.registerAnimationCallback(new Animatable2Compat.AnimationCallback() {
+                @Override
+                public void onAnimationEnd(Drawable drawable) {
+                    openMainActivity();
+                }
+            });
+            drawable.start();
+        }
+
     }
 
     @Override
@@ -38,5 +76,9 @@ public class SplashActivity extends BaseActivity<ActivitySplashBinding, SplashVi
     @Override
     public SplashViewModel getViewModel() {
         return viewModel;
+    }
+
+    private void openMainActivity(){
+        Log.i("TAG", "openMainActivity: ");
     }
 }
